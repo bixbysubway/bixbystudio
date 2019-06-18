bb3f0981799a4126bfb910642058cb7db0ba5e50
Geunyoung Kim <emsud@inu.ac.kr>
2019-06-18 22:07:41 +0900

    경과시간만 뜨도록 해놓은 버전

---
+++ bixby-workspace/example.subway/code/findPath.js
@@ -0,0 +1,139 @@
+module.exports.function = function findPath (startPoint, endPoint) {
+  const console = require('console')
+  function makeStation() {
+    const stationData = require("./vertices.js");
+    const result = {};
+
+    for (let i = 0; i < stationData.length; i++) {
+      var station_name = stationData[i].station_nm;
+      result[station_name] = {};
+      result[station_name]['line_num'] = stationData[i].line_num;
+      result[station_name]['time'] = {};
+    }
+
+    const edgeData = require("./edges.js");
+
+    for (let i = 0; i < edgeData.length; i++) {
+      var station_from = edgeData[i].from;
+      var station_to = edgeData[i].to;
+      var station_time = edgeData[i].time;
+      result[station_from]['time'][station_to] = station_time;
+      result[station_to]['time'][station_from] = station_time;
+    }
+
+    for (let i in result) {
+      if (Object.keys(result[i]['time']).length > 2) {
+        result[i]['transfer'] = "true";
+      } else {
+        result[i]['transfer'] = "false";
+      }
+    }
+
+    return result;
+  }
+
+
+  function PriorityQueue() {
+    this._nodes = [];
+
+    this.enqueue = function (priority, key) {
+      this._nodes.push({
+        key: key,
+        priority: priority
+      });
+      this.sort();
+    };
+    this.dequeue = function () {
+      return this._nodes.shift().key;
+    };
+    this.sort = function () {
+      this._nodes.sort(function (a, b) {
+        return a.priority - b.priority;
+      });
+    };
+    this.isEmpty = function () {
+      return !this._nodes.length;
+    };
+  }
+
+
+  function Graph() {
+    var INFINITY = 1 / 0;
+    this.vertices = {};
+
+    this.addVertex = function (graph) {
+      this.vertices = graph;
+    };
+
+    this.shortestPath = function (start, finish) {
+      var nodes = new PriorityQueue(),
+        distances = {},
+        previous = {},
+        path = [],
+        smallest, vertex, neighbor, alt;
+
+    for (vertex in this.vertices) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(0, vertex);
+      } else {
+        distances[vertex] = INFINITY;
+        nodes.enqueue(INFINITY, vertex);
+      }
+
+      previous[vertex] = null;
+    }
+
+    while (!nodes.isEmpty()) {
+      smallest = nodes.dequeue();
+
+      if (smallest === finish) {
+        path = [];
+
+        while (previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+
+        break;
+      }
+
+      if (!smallest || distances[smallest] === INFINITY) {
+        continue;
+      }
+
+      for (neighbor in this.vertices[smallest]['time']) {
+        alt = distances[smallest] + this.vertices[smallest]['time'][neighbor];
+
+        if (alt < distances[neighbor]) {
+          distances[neighbor] = alt;
+          previous[neighbor] = smallest;
+
+          nodes.enqueue(alt, neighbor);
+        }
+      }
+    }
+
+      return path;
+    };
+  }
+
+  var g = new Graph();
+
+  g.addVertex(makeStation());
+
+  function pathTime(path) {
+    let station = makeStation();
+    var time = 0;
+    for (let i = 0; i < path.length - 1; i++) {
+      time += station[path[i]]['time'][path[i + 1]];
+    }
+    return time;
+  }
+  
+  console.log(pathTime(g.shortestPath('동막', '문학경기장').concat('동막').reverse()));
+  var result = pathTime(g.shortestPath(startPoint, endPoint).concat(startPoint).reverse());
+  return {
+    arrivaltime:result
+  }
+}
---
+++ bixby-workspace/example.subway/code/vertices.js
@@ -0,0 +1,300 @@
+module.exports = [
+  {
+    "station_nm" : "퇴계원",
+    "line_num" : "G",
+    "fr_code" : "P125",
+    "station_cd" : "1314",
+    "xpoint_wgs" : 37.648311,
+    "ypoint_wgs" : 127.143952,
+    "station_nm_han" : "退溪院",
+    "station_nm_eng" : "Toegyewon"
+  }, {
+    "station_nm" : "평내호평",
+    "line_num" : "G",
+    "fr_code" : "P128",
+    "station_cd" : "1317",
+    "xpoint_wgs" : 37.653225,
+    "ypoint_wgs" : 127.244493,
+    "station_nm_han" : "坪內好坪",
+    "station_nm_eng" : "PyeongnaeHopyeong"
+  }, {
+    "station_nm" : "회기",
+    "line_num" : "G",
+    "fr_code" : "P117",
+    "station_cd" : "1307",
+    "xpoint_wgs" : 37.58946,
+    "ypoint_wgs" : 127.057583,
+    "station_nm_han" : "回基",
+    "station_nm_eng" : "Hoegi"
+  }, {
+    "station_nm" : "간석오거리",
+    "line_num" : "I",
+    "fr_code" : "I123",
+    "station_cd" : "3123",
+    "xpoint_wgs" : 37.467048,
+    "ypoint_wgs" : 126.707938,
+    "station_nm_han" : "間石五거리",
+    "station_nm_eng" : "Ganseogogeori"
+  }, {
+    "station_nm" : "갈산",
+    "line_num" : "I",
+    "fr_code" : "I117",
+    "station_cd" : "3117",
+    "xpoint_wgs" : 37.517268,
+    "ypoint_wgs" : 126.721514,
+    "station_nm_han" : "葛山",
+    "station_nm_eng" : "Galsan"
+  }, {
+    "station_nm" : "경인교대입구",
+    "line_num" : "I",
+    "fr_code" : "I115",
+    "station_cd" : "3115",
+    "xpoint_wgs" : 37.538157,
+    "ypoint_wgs" : 126.722597,
+    "station_nm_han" : "京仁敎大入口",
+    "station_nm_eng" : "Gyeongin National Univ. of Education"
+  }, {
+    "station_nm" : "계산",
+    "line_num" : "I",
+    "fr_code" : "I114",
+    "station_cd" : "3114",
+    "xpoint_wgs" : 37.543238,
+    "ypoint_wgs" : 126.728128,
+    "station_nm_han" : "��山",
+    "station_nm_eng" : "Gyesan"
+  }, {
+    "station_nm" : "계양",
+    "line_num" : "I",
+    "fr_code" : "I110",
+    "station_cd" : "3110",
+    "xpoint_wgs" : 37.571462,
+    "ypoint_wgs" : 126.735637,
+    "station_nm_han" : "桂陽",
+    "station_nm_eng" : "Gyeyang"
+  }, {
+    "station_nm" : "국제업무지구",
+    "line_num" : "I",
+    "fr_code" : "I138",
+    "station_cd" : "3138",
+    "xpoint_wgs" : 37.399907,
+    "ypoint_wgs" : 126.630347,
+    "station_nm_han" : "國際業務地區",
+    "station_nm_eng" : "Int'l Business District"
+  }, {
+    "station_nm" : "귤현",
+    "line_num" : "I",
+    "fr_code" : "I111",
+    "station_cd" : "3111",
+    "xpoint_wgs" : 37.566379,
+    "ypoint_wgs" : 126.742654,
+    "station_nm_han" : "橘峴",
+    "station_nm_eng" : "Gyulhyeon"
+  }, {
+    "station_nm" : "동막",
+    "line_num" : "I",
+    "fr_code" : "I132",
+    "station_cd" : "3132",
+    "xpoint_wgs" : 37.397878,
+    "ypoint_wgs" : 126.674005,
+    "station_nm_han" : "東幕",
+    "station_nm_eng" : "Dongmak"
+  }, {
+    "station_nm" : "동수",
+    "line_num" : "I",
+    "fr_code" : "I121",
+    "station_cd" : "3121",
+    "xpoint_wgs" : 37.485312,
+    "ypoint_wgs" : 126.718247,
+    "station_nm_han" : "東樹",
+    "station_nm_eng" : "Dongsu"
+  }, {
+    "station_nm" : "동춘",
+    "line_num" : "I",
+    "fr_code" : "I131",
+    "station_cd" : "3131",
+    "xpoint_wgs" : 37.404737,
+    "ypoint_wgs" : 126.681015,
+    "station_nm_han" : "東春",
+    "station_nm_eng" : "Dongchun"
+  }, {
+    "station_nm" : "문학경기장",
+    "line_num" : "I",
+    "fr_code" : "I127",
+    "station_cd" : "3127",
+    "xpoint_wgs" : 37.434935,
+    "ypoint_wgs" : 126.698579,
+    "station_nm_han" : "文鶴競技場",
+    "station_nm_eng" : "Munhak Sports Complex"
+  }, {
+    "station_nm" : "박촌",
+    "line_num" : "I",
+    "fr_code" : "I112",
+    "station_cd" : "3112",
+    "xpoint_wgs" : 37.553703,
+    "ypoint_wgs" : 126.745077,
+    "station_nm_han" : "朴村",
+    "station_nm_eng" : "Bakchon"
+  }, {
+    "station_nm" : "부평",
+    "line_num" : "I",
+    "fr_code" : "I120",
+    "station_cd" : "3120",
+    "xpoint_wgs" : 37.489493,
+    "ypoint_wgs" : 126.724805,
+    "station_nm_han" : "富平",
+    "station_nm_eng" : "Bupyeong"
+  }, {
+    "station_nm" : "부평구청",
+    "line_num" : "I",
+    "fr_code" : "I118",
+    "station_cd" : "3118",
+    "xpoint_wgs" : 37.508336,
+    "ypoint_wgs" : 126.720548,
+    "station_nm_han" : "富平區廳",
+    "station_nm_eng" : "Bupyeong-gu Office"
+  }, {
+    "station_nm" : "부평삼거리",
+    "line_num" : "I",
+    "fr_code" : "I122",
+    "station_cd" : "3122",
+    "xpoint_wgs" : 37.477679,
+    "ypoint_wgs" : 126.710208,
+    "station_nm_han" : "富平三거리",
+    "station_nm_eng" : "Bupyeongsamgeori"
+  }, {
+    "station_nm" : "부평시장",
+    "line_num" : "I",
+    "fr_code" : "I119",
+    "station_cd" : "3119",
+    "xpoint_wgs" : 37.498383,
+    "ypoint_wgs" : 126.722244,
+    "station_nm_han" : "富平市場",
+    "station_nm_eng" : "Bupyeong Market"
+  }, {
+    "station_nm" : "선학",
+    "line_num" : "I",
+    "fr_code" : "I128",
+    "station_cd" : "3128",
+    "xpoint_wgs" : 37.426684,
+    "ypoint_wgs" : 126.698863,
+    "station_nm_han" : "仙鶴",
+    "station_nm_eng" : "Seonhak"
+  }, {
+    "station_nm" : "센트럴파크",
+    "line_num" : "I",
+    "fr_code" : "I137",
+    "station_cd" : "3137",
+    "xpoint_wgs" : 37.393054,
+    "ypoint_wgs" : 126.634729,
+    "station_nm_han" : "센트럴파크",
+    "station_nm_eng" : "Central Park"
+  }, {
+    "station_nm" : "신연수",
+    "line_num" : "I",
+    "fr_code" : "I129",
+    "station_cd" : "3129",
+    "xpoint_wgs" : 37.41804,
+    "ypoint_wgs" : 126.693863,
+    "station_nm_han" : "新延壽",
+    "station_nm_eng" : "Sinyeonsu"
+  }, {
+    "station_nm" : "예술회관",
+    "line_num" : "I",
+    "fr_code" : "I125",
+    "station_cd" : "3125",
+    "xpoint_wgs" : 37.449396,
+    "ypoint_wgs" : 126.701012,
+    "station_nm_han" : "藝術會館",
+    "station_nm_eng" : "Arts Center"
+  }, {
+    "station_nm" : "원인재",
+    "line_num" : "I",
+    "fr_code" : "I130",
+    "station_cd" : "3130",
+    "xpoint_wgs" : 37.412603,
+    "ypoint_wgs" : 126.687389,
+    "station_nm_han" : "源仁齋",
+    "station_nm_eng" : "Woninjae"
+  }, {
+    "station_nm" : "인천대입구",
+    "line_num" : "I",
+    "fr_code" : "I136",
+    "station_cd" : "3136",
+    "xpoint_wgs" : 37.386007,
+    "ypoint_wgs" : 126.639484,
+    "station_nm_han" : "仁川大入口",
+    "station_nm_eng" : "Univ.of Incheon"
+  }, {
+    "station_nm" : "인천시청",
+    "line_num" : "I",
+    "fr_code" : "I124",
+    "station_cd" : "3124",
+    "xpoint_wgs" : 37.457405,
+    "ypoint_wgs" : 126.702221,
+    "station_nm_han" : "仁川市廳",
+    "station_nm_eng" : "Incheon City Hall"
+  }, {
+    "station_nm" : "인천터미널",
+    "line_num" : "I",
+    "fr_code" : "I126",
+    "station_cd" : "3126",
+    "xpoint_wgs" : 37.442383,
+    "ypoint_wgs" : 126.699706,
+    "station_nm_han" : "仁川터미널",
+    "station_nm_eng" : "Incheon Bus Terminal"
+  }, {
+    "station_nm" : "임학",
+    "line_num" : "I",
+    "fr_code" : "I113",
+    "station_cd" : "3113",
+    "xpoint_wgs" : 37.545059,
+    "ypoint_wgs" : 126.738665,
+    "station_nm_han" : "林鶴",
+    "station_nm_eng" : "Imhak"
+  }, {
+    "station_nm" : "작전",
+    "line_num" : "I",
+    "fr_code" : "I116",
+    "station_cd" : "3116",
+    "xpoint_wgs" : 37.530415,
+    "ypoint_wgs" : 126.722527,
+    "station_nm_han" : "鵲田",
+    "station_nm_eng" : "Jakjeon"
+  }, {
+    "station_nm" : "지식정보단지",
+    "line_num" : "I",
+    "fr_code" : "I135",
+    "station_cd" : "3135",
+    "xpoint_wgs" : 37.378384,
+    "ypoint_wgs" : 126.645168,
+    "station_nm_han" : "知識情報團地",
+    "station_nm_eng" : "BIT Zone"
+  }, {
+    "station_nm" : "캠퍼스타운",
+    "line_num" : "I",
+    "fr_code" : "I133",
+    "station_cd" : "3133",
+    "xpoint_wgs" : 37.387855,
+    "ypoint_wgs" : 126.661673,
+    "station_nm_han" : "캠퍼스타운",
+    "station_nm_eng" : "Campus Town"
+  }, {
+    "station_nm" : "테크노파크",
+    "line_num" : "I",
+    "fr_code" : "I134",
+    "station_cd" : "3134",
+    "xpoint_wgs" : 37.382268,
+    "ypoint_wgs" : 126.656365,
+    "station_nm_han" : "테크노파크",
+    "station_nm_eng" : "Technopark"
+  }, {
+    "station_nm" : "인천시청",
+    "line_num" : "I2",
+    "fr_code" : "I221",
+    "station_cd" : "3221",
+    "xpoint_wgs" : 37.457405,
+    "ypoint_wgs" : 126.702221,
+    "station_nm_han" : "仁川市廳",
+    "station_nm_eng" : "Incheon City Hall"
+  },
+]
